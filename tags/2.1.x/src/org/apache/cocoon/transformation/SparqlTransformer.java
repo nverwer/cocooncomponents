cf41f802cfa18a74c57a4552a0c72f08c4a8da7d
nverwer <nverwer@a68e285e-1d3b-0410-a1fb-3389c1723150>
2010-08-18 12:57:10 +0000

    A much better SparqlTransformer, which uses http-headers. This one works with openrdf-sesame.

--- tags/2.1.x/src/org/apache/cocoon/transformation/SparqlTransformer.java
+++ tags/2.1.x/src/org/apache/cocoon/transformation/SparqlTransformer.java
@@ -17,15 +17,33 @@
 package org.apache.cocoon.transformation;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Properties;
 
+import javax.xml.transform.OutputKeys;
+
+import org.apache.avalon.framework.component.Component;
+import org.apache.avalon.framework.component.ComponentException;
 import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.avalon.framework.service.ServiceException;
 import org.apache.cocoon.ProcessingException;
 import org.apache.cocoon.components.source.SourceUtil;
 import org.apache.cocoon.environment.SourceResolver;
-import org.apache.cocoon.xml.AttributesImpl;
-import org.apache.excalibur.source.Source;
+import org.apache.cocoon.xml.IncludeXMLConsumer;
+import org.apache.cocoon.xml.XMLUtils;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.*;
+import org.apache.excalibur.source.SourceException;
 import org.apache.excalibur.source.SourceParameters;
+import org.apache.excalibur.xmlizer.XMLizer;
+import org.apache.webdav.lib.methods.HttpRequestBodyMethodBase;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 
@@ -33,17 +51,21 @@ import org.xml.sax.SAXException;
  *  * @cocoon.sitemap.component.documentation
  * This transformer triggers for the element <code>query</code> in the namespace "http://apache.org/cocoon/sparql/1.0".
  * These elements must not be nested.
- * The <code>src</code> attribute contains the url which points to a SPARQL endpoint.
- * The optional <code>method</code> attribute contains the HTTP method for the request (default is GET).
- * The optional <code>encoding</code> attribute contains the HTTP encoding for the request (default is ISO-8859-1).
- * Attributes in the "http://apache.org/cocoon/sparql/1.0" namespace are used as request parameters (using the local name).
- * This allows for parameters such as 'format' or 'maxrows'.
- * The text of the content of the <code>query</code> element is passed as the value of the 'query' parameter.
+ * The mandatory <code>src</code> attribute contains the url of a SPARQL endpoint.
+ * The optional <code>method</code> attribute contains the HTTP method for the request (default is GET). AT the moment, we only do GET and POST.
+ * The optional <code>parse</code> attribute indicates how the response should be parsed. It can be "xml" or "text". Default is "xml".
+ * Attributes in the "http://www.w3.org/2006/http#" namespace are used as request headers. The header name is the local name of the attribute.
+ * Attributes in the "http://apache.org/cocoon/sparql/1.0" namespace are used as request parameters. The parameter name is the local name of the attribute.
+ * The text content of the <code>query</code> element is passed as the value of the 'query' parameter.
  * 
  * The XML input to this transformer would look like:
  * <pre>
- *   <sparql:query xmlns:sparql="http://apache.org/cocoon/sparql/1.0"
- *     method="POST" src="http://dbpedia.org/sparql"
+ *   <sparql:query
+ *     xmlns:sparql="http://apache.org/cocoon/sparql/1.0"
+ *     xmlns:http="http://www.w3.org/2006/http#"
+ *     src="http://dbpedia.org/sparql"
+ *     method="POST"
+ *     http:accept="application/sparql-results+xml"
  *     sparql:maxrows="25" sparql:format="XML"
  *   >
  *   <![CDATA[
@@ -57,22 +79,24 @@ import org.xml.sax.SAXException;
  *   </sparql:query>
  * </pre>
  * 
- * @author Nico Verwer
+ * @author Nico Verwer (nico.verwer@withart.com)
  *
  */
 public class SparqlTransformer extends AbstractSAXTransformer {
   
   public static final String SPARQL_NAMESPACE_URI = "http://apache.org/cocoon/sparql/1.0";
+  public static final String HTTP_NAMESPACE_URI ="http://www.w3.org/2006/http#";
   public static final String QUERY_ELEMENT = "query";
   public static final String METHOD_ATTR = "method";
+  public static final String PARSE_ATTR = "parse";
   public static final String SRC_ATTR = "src";
-  public static final String ENCODING_ATTR = "encoding";
   public static final String QUERY_PARAM = "query";
   
   private boolean inQuery;
   private String src;
   private String method;
-  private String encoding;
+  private String parse;
+  private Map httpHeaders;
   private SourceParameters requestParameters;
 
   public SparqlTransformer() {
@@ -93,13 +117,17 @@ public class SparqlTransformer extends AbstractSAXTransformer {
       }
       inQuery = true;
       src = attr.getValue(SRC_ATTR);
+      if (src == null) throw new ProcessingException("The "+SRC_ATTR+" attribute is mandatory for "+QUERY_ELEMENT+" elements.");
       method = attr.getValue(METHOD_ATTR);
       if (method == null) method = "GET";
-      encoding = attr.getValue(ENCODING_ATTR);
-      if (encoding == null) encoding = System.getProperty("file.encoding", "ISO-8859-1"); 
+      parse = attr.getValue(PARSE_ATTR);
+      if (parse == null) parse = "xml";
       requestParameters = new SourceParameters();
+      httpHeaders = new HashMap();
       for (int i = 0; i < attr.getLength(); ++i) {
-        if (attr.getURI(i).equals(SPARQL_NAMESPACE_URI)) {
+        if (attr.getURI(i).equals(HTTP_NAMESPACE_URI)) {
+          httpHeaders.put(attr.getLocalName(i), attr.getValue(i));
+        } else if (attr.getURI(i).equals(SPARQL_NAMESPACE_URI)) {
           requestParameters.setParameter(attr.getLocalName(i), attr.getValue(i));
         }
       }
@@ -113,13 +141,66 @@ public class SparqlTransformer extends AbstractSAXTransformer {
       inQuery = false;
       String query = endTextRecording();
       requestParameters.setParameter(QUERY_PARAM, query);
-      Parameters typeParameters = new Parameters();
-      typeParameters.setParameter("method", method);
-      typeParameters.setParameter("encoding", encoding);
-      //typeParameters.setParameter("mime-type", ...) // mime-type hint
-      Source source = SourceUtil.getSource(src, typeParameters, requestParameters, resolver);
-      SourceUtil.toSAX(source, this.xmlConsumer, typeParameters, true);
+      executeRequest(src, method, httpHeaders, requestParameters);
     }
   }
-  
+
+  private void executeRequest(String url, String method, Map httpHeaders, SourceParameters requestParameters)
+      throws ProcessingException, IOException, SAXException {
+    HttpClient httpclient = new HttpClient();
+    HttpMethod httpMethod = null;
+    if ("GET".equalsIgnoreCase(method)) {
+      httpMethod = new GetMethod(url);
+      httpMethod.setQueryString(requestParameters.getEncodedQueryString());
+    } else if ("POST".equalsIgnoreCase(method)) {
+      PostMethod httpPostMethod = new PostMethod(url);
+      Iterator parNames = requestParameters.getParameterNames();
+      while (parNames.hasNext()) {
+        String parName = (String) parNames.next();
+        httpPostMethod.addParameter(parName, requestParameters.getParameter(parName));
+      }
+      httpMethod = httpPostMethod;
+    }
+    // Add request headers.
+    Iterator headers = httpHeaders.entrySet().iterator();
+    while (headers.hasNext()) {
+      Map.Entry header = (Map.Entry) headers.next();
+      httpMethod.addRequestHeader((String) header.getKey(), (String) header.getValue());
+    }
+    // Execute the request.
+    int responseCode;
+    try {
+      responseCode = httpclient.executeMethod(httpMethod);
+    } catch (HttpException e) {
+      throw new IOException(e);
+    }
+    if (responseCode < 200 || responseCode >= 300)
+      throw new ProcessingException("Received HTTP status code "+responseCode);
+    // Parse the response
+    XMLizer xmlizer = null;
+    try {
+      xmlizer = (XMLizer) manager.lookup(XMLizer.ROLE);
+      if (parse.equalsIgnoreCase("xml")) {
+        xmlizer.toSAX(httpMethod.getResponseBodyAsStream(),
+            "text/xml", httpMethod.getURI().toString(),
+            new IncludeXMLConsumer(xmlConsumer));
+      } else if (parse.equalsIgnoreCase("text")) {
+        xmlConsumer.startElement("http://apache.org/cocoon/sparql/1.0", "result", "sparql:result", EMPTY_ATTRIBUTES);
+        String responseBody = httpMethod.getResponseBodyAsString();
+        xmlConsumer.characters(responseBody.toCharArray(), 0, responseBody.length());
+        xmlConsumer.endElement("http://apache.org/cocoon/sparql/1.0", "result", "sparql:result");
+      } else {
+        throw new ProcessingException("Unknown parse type: "+parse);
+      }
+    } catch (ServiceException e) {
+      throw new ProcessingException("Cannot find the right XMLizer for "+XMLizer.ROLE, e);
+    } finally {
+        manager.release((Component) xmlizer);
+    }
+//    Parameters typeParameters = new Parameters();
+//    typeParameters.setParameter("method", method);
+//    Source source = SourceUtil.getSource(src, typeParameters, requestParameters, resolver);
+//    SourceUtil.toSAX(source, this.xmlConsumer, typeParameters, true);
+  }
+
 }
