8f741cad0296ec85fb77a6e36e51b170a93d0e57
nverwer <nverwer@a68e285e-1d3b-0410-a1fb-3389c1723150>
2012-03-19 09:36:17 +0000

    Documentation for regexp input module. Duration parameter for pipeline slowdown transformer. Increased source and target versions for javac to 1.6.

--- src/org/apache/cocoon/transformation/PipelineSlowdownTransformer.java
+++ src/org/apache/cocoon/transformation/PipelineSlowdownTransformer.java
@@ -2,6 +2,8 @@ package org.apache.cocoon.transformation;
 
 import java.io.IOException;
 import java.util.Map;
+
+import org.apache.avalon.framework.parameters.ParameterException;
 import org.apache.avalon.framework.parameters.Parameters;
 import org.apache.cocoon.ProcessingException;
 import org.apache.cocoon.environment.SourceResolver;
@@ -10,15 +12,26 @@ import org.xml.sax.SAXException;
 
 public class PipelineSlowdownTransformer extends AbstractTransformer
 {
+  int msecs = 100;
+  
   public void setup(SourceResolver resolver, Map objectModel, String src, Parameters par)
     throws ProcessingException, SAXException, IOException
   {
+    if (par.isParameter("duration")) {
+      try {
+        msecs = Integer.parseInt(par.getParameter("duration"));
+      } catch (NumberFormatException e) {
+        throw new ProcessingException(e);
+      } catch (ParameterException e) {
+        throw new ProcessingException(e);
+      }
+    }
   }
   public void startElement(String namespaceURI, String lName, String qName, Attributes attributes)
     throws SAXException
   {
     super.startElement(namespaceURI, lName, qName, attributes);
     System.err.print('.');
-    try {Thread.sleep(100);} catch (Exception e) {}
+    try {Thread.sleep(msecs);} catch (Exception e) {}
   }
 }
