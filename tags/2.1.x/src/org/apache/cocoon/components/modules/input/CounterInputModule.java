8a873f622c0d1d093b094fb75ff5e40f2801d987
hhverweij@gmail.com <hhverweij@gmail.com@a68e285e-1d3b-0410-a1fb-3389c1723150>
2013-05-03 08:46:41 +0000

    Verhoogt een teller bij elke aanroep (sitemap match). Bijv. <map:transform type="tee" src="tee-a-{counter:mycounter}"/>.

---
+++ tags/2.1.x/src/org/apache/cocoon/components/modules/input/CounterInputModule.java
@@ -0,0 +1,70 @@
+package org.apache.cocoon.components.modules.input;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.*;
+
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.avalon.framework.thread.ThreadSafe;
+import org.apache.cocoon.components.modules.input.AbstractInputModule;
+import org.apache.cocoon.environment.ObjectModelHelper;
+import org.apache.cocoon.environment.http.HttpCookie;
+import org.apache.regexp.RE;
+
+public class CounterInputModule extends AbstractInputModule implements ThreadSafe {
+  
+  private HashMap counters = new HashMap();
+  
+  /**
+   * @return Increment the value of a counter on every call.
+   * For example, suppose you call this module "counter", and you have a
+   * counter called "my-match-1" that does not exist yet.
+   * Then in your sitemap, {counter:my-match-1}
+   * will yield the value "1". Subsequent matches will return 1, 2, 3, etc.
+   */
+    public Object getAttribute(String attribute, Configuration modeConf, Map objectModel)
+            throws ConfigurationException {
+        Integer value = (Integer) this.counters.get(attribute);
+        if (null == value) {
+            this.counters.put(attribute, new Integer(1));
+        } else {
+            this.counters.put(attribute, new Integer(value.intValue() + 1));
+        }
+        System.out.println("Counter[" + attribute + "]=" + this.counters.get(attribute));
+        getLogger().info("Counter[" + attribute + "]=" + this.counters.get(attribute));
+        if (getLogger().isDebugEnabled()) {
+            getLogger().debug("Counter[" + attribute + "]=" + this.counters.get(attribute));
+        }
+        return this.counters.get(attribute);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * org.apache.cocoon.components.modules.input.InputModule#getAttributeNames(org.apache.avalon.framework.configuration.Configuration,
+     * java.util.Map)
+     */
+    public Iterator getAttributeNames(Configuration modeConf, Map objectModel)
+            throws ConfigurationException {
+
+        return this.counters.entrySet().iterator();
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * org.apache.cocoon.components.modules.input.InputModule#getAttributeValues(java.lang.String,
+     * org.apache.avalon.framework.configuration.Configuration, java.util.Map)
+     */
+    public Object[] getAttributeValues(String name, Configuration modeConf,
+            Map objectModel) throws ConfigurationException {
+
+        Object[] objects = new Object[1];
+        objects[1] = this.counters.get(name);
+        return objects;
+    }
+
+}
