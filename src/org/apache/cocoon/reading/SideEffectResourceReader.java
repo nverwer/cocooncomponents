b10b2699b1509490da34f31f77909802b164de61
nverwer <nverwer@a68e285e-1d3b-0410-a1fb-3389c1723150>
2013-07-11 13:37:06 +0000

    SideEffectResourceReader.java

--- src/org/apache/cocoon/reading/SideEffectResourceReader.java
+++ src/org/apache/cocoon/reading/SideEffectResourceReader.java
@@ -1,8 +1,7 @@
 package org.apache.cocoon.reading;
 
-import java.io.IOException;
+import java.io.*;
 
-import org.apache.avalon.framework.parameters.ParameterException;
 import org.apache.cocoon.ProcessingException;
 
 public class SideEffectResourceReader extends ResourceReader{
@@ -13,7 +12,12 @@ public class SideEffectResourceReader extends ResourceReader{
         String sideEffectURL;
         try {
             sideEffectURL = parameters.getParameter("side-effect-uri");
-            resolver.resolveURI(sideEffectURL);
+            org.apache.excalibur.source.Source se_source = resolver.resolveURI(sideEffectURL);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(se_source.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
         } catch (Exception e) {
             getLogger().error("Cannot resolve the URL for side-effect:"+e.toString());
         }
