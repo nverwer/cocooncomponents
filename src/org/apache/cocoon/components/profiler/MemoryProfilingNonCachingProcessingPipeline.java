140ee27229e14c2bab3f07293179621f99056dcc
Nico Verwer <nverwer@rakensi.com>
2015-10-19 16:24:06 +0200

    memory-measuring profiling pipeline processor

--- src/org/apache/cocoon/components/profiler/MemoryProfilingNonCachingProcessingPipeline.java
+++ src/org/apache/cocoon/components/profiler/MemoryProfilingNonCachingProcessingPipeline.java
@@ -38,6 +38,8 @@ import org.xml.sax.SAXException;
 /**
  * Special version of the NonCachingProcessingPipeline that measures heap memory
  * usage of pipelines. Based on ProfilingNonCachingProcessingPipeline.
+ * The total memory usage in bytes will be the value of the 'time' attribute in the profile.
+ * In this way, the existing profiling componets can be used.
  * 
  * @author <a href="mailto:nverwer@rakensi.com">Nico Verwer</a>
  * @author <a href="mailto:vgritsenko@apache.org">Vadim Gritsenko</a>
@@ -49,7 +51,6 @@ public class MemoryProfilingNonCachingProcessingPipeline
 
   private Profiler profiler;
   private ProfilerData data;
-  private int index;
   private List<MemoryPoolMXBean> memoryMX;
 
   /**
@@ -77,153 +78,23 @@ public class MemoryProfilingNonCachingProcessingPipeline
    */
   public void recycle() {
     this.data = null;
-    this.index = 0;
     super.recycle();
   }
   
-  private long initMemoryMeasurement() {
-    long memoryInUse = 0;
+  private void initMemoryMeasurement() {
     System.gc();
     for (MemoryPoolMXBean mmx : this.memoryMX) {
       mmx.resetPeakUsage();
-      memoryInUse += mmx.getPeakUsage().getUsed();
     }
-    return memoryInUse;
   }
   
-  private long currentMemoryMeasurement() {
+  private long getMemoryMeasurement() {
     long memoryInUse = 0;
     for (MemoryPoolMXBean mmx : this.memoryMX) {
       memoryInUse += mmx.getPeakUsage().getUsed();
     }
     return memoryInUse;
   }
-
-  /**
-   * Set the generator that will be used as the initial step in the pipeline.
-   *
-   * @param role
-   *          the generator role in the component manager.
-   * @param source
-   *          the source where to produce XML from, or <code>null</code> if no
-   *          source is given.
-   * @param param
-   *          the parameters for the generator.
-   * @param hintParam
-   * @throws ProcessingException
-   *          if the generator couldn't be obtained.
-   */
-  public void setGenerator(String role, String source, Parameters param, Parameters hintParam) throws ProcessingException {
-    super.setGenerator(role, source, param, hintParam);
-/*    if (this.data == null) {
-      this.data = new ProfilerData();
-    }
-    this.data.addComponent(super.generator, role, source);*/
-  }
-
-  /**
-   * Add a transformer at the end of the pipeline.
-   *
-   * @param role
-   *          the transformer role in the component manager.
-   * @param source
-   *          the source used to setup the transformer (e.g. XSL file), or
-   *          <code>null</code> if no source is given.
-   * @param param
-   *          the parameters for the transfomer.
-   * @param hintParam
-   * @throws ProcessingException
-   *           if the generator couldn't be obtained.
-   */
-  public void addTransformer(String role, String source, Parameters param, Parameters hintParam) throws ProcessingException {
-    super.addTransformer(role, source, param, hintParam);
-/*    if (this.data == null) {
-      this.data = new ProfilerData();
-    }
-    this.data.addComponent(
-        super.transformers.get(super.transformers.size() - 1), role, source);*/
-  }
-
-  /**
-   * Set the serializer for this pipeline
-   *
-   * @param role
-   * @param source
-   * @param param
-   * @param hintParam
-   * @param mimeType
-   */
-  public void setSerializer(String role, String source, Parameters param,
-      Parameters hintParam, String mimeType) throws ProcessingException {
-    super.setSerializer(role, source, param, hintParam, mimeType);
-/*    if (this.data == null) {
-      this.data = new ProfilerData();
-    }
-    this.data.addComponent(super.serializer, role, source);*/
-  }
-
-  /**
-   * Set the reader for this pipeline
-   *
-   * @param role
-   * @param source
-   * @param param
-   * @param mimeType
-   */
-  public void setReader(String role, String source, Parameters param,
-      String mimeType) throws ProcessingException {
-    super.setReader(role, source, param, mimeType);
-/*    if (this.data == null) {
-      this.data = new ProfilerData();
-    }
-    this.data.addComponent(super.reader, role, source);*/
-  }
-
-  /**
-   * Setup pipeline components.
-   *
-   * @param environment
-   */
-/*  protected void setupPipeline(Environment environment)
-      throws ProcessingException {
-    try {
-      // Setup the generator
-      long time = System.currentTimeMillis();
-      this.generator.setup(environment, environment.getObjectModel(),
-          generatorSource, generatorParam);
-      this.data.setSetupTime(0, System.currentTimeMillis() - time);
-
-      Iterator transformerItt = this.transformers.iterator();
-      Iterator transformerSourceItt = this.transformerSources.iterator();
-      Iterator transformerParamItt = this.transformerParams.iterator();
-
-      // Setup transformers
-      int index = 1;
-      while (transformerItt.hasNext()) {
-        Transformer trans = (Transformer) transformerItt.next();
-
-        time = System.currentTimeMillis();
-        trans.setup(environment, environment.getObjectModel(),
-            (String) transformerSourceItt.next(),
-            (Parameters) transformerParamItt.next());
-        this.data.setSetupTime(index++, System.currentTimeMillis() - time);
-      }
-
-      // Setup serializer
-      time = System.currentTimeMillis();
-      if (this.serializer instanceof SitemapModelComponent) {
-        ((SitemapModelComponent) this.serializer).setup(environment,
-            environment.getObjectModel(), serializerSource, serializerParam);
-      }
-      this.data.setSetupTime(index++, System.currentTimeMillis() - time);
-
-      setMimeTypeForSerializer(environment);
-    } catch (SAXException e) {
-      throw new ProcessingException("Could not setup pipeline.", e);
-    } catch (IOException e) {
-      throw new ProcessingException("Could not setup pipeline.", e);
-    }
-  }*/
 
   /**
    * Process the given <code>Environment</code>, producing the output.
@@ -233,17 +104,16 @@ public class MemoryProfilingNonCachingProcessingPipeline
    * @return true on success
    */
   public boolean process(Environment environment) throws ProcessingException {
-    this.index = 0;
     if (this.data == null) this.data = new ProfilerData();
     if (this.data != null) {
       // Capture environment info
       this.data.setEnvironmentInfo(new EnvironmentInfo(environment));
       // Execute pipeline
-      long memory = this.initMemoryMeasurement();
+      this.initMemoryMeasurement();
       boolean result = super.process(environment);
-      this.data.setTotalTime(this.currentMemoryMeasurement() - memory);
+      this.data.setTotalTime(this.getMemoryMeasurement());
       // Report
-      profiler.addResult(environment.getURI(), this.data);
+      profiler.addResult(environment.getURI()+" (process)", this.data);
       return result;
     } else {
       getLogger().warn("Profiler Data does not have any components to measure");
@@ -251,66 +121,4 @@ public class MemoryProfilingNonCachingProcessingPipeline
     }
   }
 
-  /**
-   * Process the SAX event pipeline
-   */
-  protected boolean processXMLPipeline(Environment environment)
-      throws ProcessingException {
-    this.index = 0;
-    if (this.data == null) this.data = new ProfilerData();
-    if (this.data != null) {
-      // Capture environment info
-      this.data.setEnvironmentInfo(new EnvironmentInfo(environment));
-      // Execute pipeline
-      long memory = this.initMemoryMeasurement();
-      boolean result = super.processXMLPipeline(environment);
-      this.data.setTotalTime(this.currentMemoryMeasurement() - memory);
-      // Report
-      profiler.addResult(environment.getURI(), this.data);
-      return result;
-    } else {
-      getLogger().warn("Profiler Data havn't any components to measure");
-      return super.processXMLPipeline(environment);
-    }
-  }
-
-  /**
-   * Process the pipeline using a reader.
-   */
-  protected boolean processReader(Environment environment)
-      throws ProcessingException {
-    this.index = 0;
-    if (this.data == null) this.data = new ProfilerData();
-    if (this.data != null) {
-      // Capture environment info
-      this.data.setEnvironmentInfo(new EnvironmentInfo(environment));
-      // Execute pipeline
-      long memory = this.initMemoryMeasurement();
-      boolean result = super.processReader(environment);
-      this.data.setTotalTime(this.currentMemoryMeasurement() - memory);
-      // Report
-      profiler.addResult(environment.getURI(), this.data);
-      return result;
-    } else {
-      getLogger().warn("Profiler Data havn't any components to measure");
-      return super.processReader(environment);
-    }
-  }
-
-  /**
-   * Connect the next component
-   *
-   * @param environment
-   * @param producer
-   * @param consumer
-   */
-/*  protected void connect(Environment environment, XMLProducer producer,
-      XMLConsumer consumer) throws ProcessingException {
-    ProfilingXMLPipe connector = new ProfilingXMLPipe();
-    connector.setup(this.index, this.data);
-    this.index++;
-    super.connect(environment, producer, connector);
-    super.connect(environment, connector, consumer);
-  }
-*/
 }
