9e98be80818c60e3d405523fa845f64e01105df5
Nico Verwer <nverwer@rakensi.com>
2016-03-07 17:02:18 +0100

    Attempt at better namespace prefix declaration handling.

--- src/org/apache/cocoon/transformation/AbstractSAXPipelineTransformer.java
+++ src/org/apache/cocoon/transformation/AbstractSAXPipelineTransformer.java
@@ -1,6 +1,9 @@
 package org.apache.cocoon.transformation;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.regex.Matcher;
@@ -13,8 +16,7 @@ import org.apache.cocoon.environment.SourceResolver;
 import org.apache.cocoon.transformation.helpers.TextRecorder;
 import org.apache.cocoon.xml.SaxBuffer;
 import org.apache.cocoon.xml.XMLUtils;
-import org.apache.cocoon.xml.SaxBuffer.EndElement;
-import org.apache.cocoon.xml.SaxBuffer.StartElement;
+import org.apache.excalibur.xml.sax.XMLizable;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 
@@ -25,6 +27,11 @@ import org.xml.sax.SAXException;
  * It also fixes the broken behaviour of endTextRecording().
  */
 public class AbstractSAXPipelineTransformer extends AbstractSAXTransformer {
+
+  /**
+   * The namespaces and their prefixes
+   */
+  private final List<String[]> namespaces = new ArrayList<String[]>(10);
   
   /**
    * The current prefix for our namespace.
@@ -42,13 +49,52 @@ public class AbstractSAXPipelineTransformer extends AbstractSAXTransformer {
   
   /**
    * @see org.xml.sax.ContentHandler#startPrefixMapping
+   * ourPrefix is private to AbstractSAXTransformer, so we have to duplicate it to use it here.
    */
-  public void startPrefixMapping(String prefix, String uri)
-  throws SAXException {
+  public void startPrefixMapping(String prefix, String uri) throws SAXException {
+    if (prefix != null) {
+      this.namespaces.add(new String[] {prefix, uri});
+    }
     if (this.namespaceURI.equals(uri)) {
       this.ourPrefix = prefix;
     }
     super.startPrefixMapping(prefix, uri);
+  }
+
+  /**
+   * Process the SAX event.
+   * @see org.xml.sax.ContentHandler#endPrefixMapping
+   * namespaces is private to AbstractSAXTransformer, so we have to duplicate it to use it here.
+   */
+  public void endPrefixMapping(String prefix) throws SAXException {
+    if (prefix != null) {
+      // Find and remove the namespace prefix
+      boolean found = false;
+      for (int i = this.namespaces.size() - 1; i >= 0; i--) {
+        final String[] prefixAndUri = (String[]) this.namespaces.get(i);
+        if (prefixAndUri[0].equals(prefix)) {
+          this.namespaces.remove(i);
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        throw new SAXException("Namespace for prefix '" + prefix + "' not found.");
+      }
+      if (prefix.equals(this.ourPrefix)) {
+        // Reset our current prefix
+        this.ourPrefix = null;
+        // Now search if we have a different prefix for our namespace
+        for (int i = this.namespaces.size() - 1; i >= 0; i--) {
+          final String[] prefixAndUri = (String[]) this.namespaces.get(i);
+          if (namespaceURI.equals(prefixAndUri[1])) {
+            this.ourPrefix = prefixAndUri[0];
+            break;
+          }
+        }
+      }
+    }
+    super.endPrefixMapping(prefix);
   }
 
   /**
@@ -66,12 +112,43 @@ public class AbstractSAXPipelineTransformer extends AbstractSAXTransformer {
   }
 
   /**
+   * Return the serialized xml string.
+   * @return A string containing the recorded xml information, formatted by
+   * the properties passed to the corresponding startSerializedXMLRecording().
+   */
+  public String endSerializedXMLRecording()
+  throws SAXException, ProcessingException {
+      XMLizable xml = endSAXRecording();
+      String text = XMLUtils.serialize(xml, (Properties) this.stack.pop());
+      if (getLogger().isDebugEnabled()) {
+          getLogger().debug("End serialized XML recording. XML=" + text);
+      }
+      return text;
+  }
+
+  /**
    * Start recording of SAX events. All incoming events are recorded and not forwarded.
    * The resulting XMLizable can be obtained by the matching {@link #endSAXRecording} call.
    */
   public void startSAXRecording()
   throws SAXException {
     addRecorder(new NamespacePrefixRepeatingSaxBuffer(this));
+    /* Old version, overridden:
+    addRecorder(new SaxBuffer());
+    sendStartPrefixMapping();
+    */
+  }
+
+  /**
+   * Stop recording of SAX events.
+   * This method returns the resulting XMLizable.
+   */
+  public XMLizable endSAXRecording()
+  throws SAXException {
+    /* Seems to be unnecessary with NamespacePrefixRepeatingSaxBuffer
+    sendEndPrefixMapping();
+    */
+    return (XMLizable) removeRecorder();
   }
 
   /**
@@ -166,6 +243,38 @@ public class AbstractSAXPipelineTransformer extends AbstractSAXTransformer {
     String name = this.ourPrefix != null ? this.ourPrefix + ':' + localname : localname;
     contentHandler.endElement(this.namespaceURI, localname, name);
   }
+
+  /**
+   * Send all start prefix mapping events to the current content handler
+   */
+  protected void sendStartPrefixMapping() throws SAXException {
+    Map<String, String> prefixAndUris = uniqueNamespacePrefixes();
+    for (String prefix : prefixAndUris.keySet()) {
+      String uri = prefixAndUris.get(prefix);
+      contentHandler.startPrefixMapping(prefix, uri);
+    }
+  }
+
+  /**
+   * Send all end prefix mapping events to the current content handler
+   */
+  protected void sendEndPrefixMapping() throws SAXException {
+    Map<String, String> prefixAndUris = uniqueNamespacePrefixes();
+    for (String prefix : prefixAndUris.keySet()) {
+      contentHandler.endPrefixMapping(prefix);
+    }
+  }
+  
+  private Map<String, String> uniqueNamespacePrefixes() {
+    Map<String, String> prefixAndUris = new HashMap<String, String>();
+    // Go through namespaces in order, so newer declarations for a prefix overwrite older ones.
+    final int l = this.namespaces.size();
+    for (int i = 0; i < l; i++) {
+      String[] prefixAndUri = (String[]) this.namespaces.get(i);
+      prefixAndUris.put(prefixAndUri[0], prefixAndUri[1]);
+    }
+    return prefixAndUris;
+  }
   
   /**
    * Way too simple parser to interpolate input module expressions.
@@ -192,9 +301,9 @@ public class AbstractSAXPipelineTransformer extends AbstractSAXTransformer {
   public class NamespacePrefixRepeatingSaxBuffer extends SaxBuffer {
     private static final long serialVersionUID = 1L;
     
-    private AbstractSAXTransformer transformer;
+    private AbstractSAXPipelineTransformer transformer;
     
-    public NamespacePrefixRepeatingSaxBuffer(AbstractSAXTransformer transformer) {
+    public NamespacePrefixRepeatingSaxBuffer(AbstractSAXPipelineTransformer transformer) {
       this.transformer = transformer;
     }
 
