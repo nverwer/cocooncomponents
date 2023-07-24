7d70951c408ffbefaf070037f290029e7b983fc7
Huib Verweij <hhv@x-scale.com>
2023-07-24 16:58:53 +0200

    PipelineSlowdownTransformerJustOnce die slechts één keer pauzeert en niet bij elk element.

---
+++ src/org/apache/cocoon/transformation/PipelineSlowdownTransformerJustOnce.java
@@ -0,0 +1,37 @@
+package org.apache.cocoon.transformation;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.avalon.framework.parameters.ParameterException;
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.cocoon.ProcessingException;
+import org.apache.cocoon.environment.SourceResolver;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+
+public class PipelineSlowdownTransformerJustOnce extends AbstractTransformer {
+  int msecs = 100;
+
+  public void setup(SourceResolver resolver, Map objectModel, String src, Parameters par)
+          throws ProcessingException, SAXException, IOException {
+    if (par.isParameter("duration")) {
+      try {
+        msecs = Integer.parseInt(par.getParameter("duration"));
+      } catch (NumberFormatException e) {
+        throw new ProcessingException(e);
+      } catch (ParameterException e) {
+        throw new ProcessingException(e);
+      }
+    }
+  }
+
+  public void startDocument() throws SAXException {
+    super.startDocument();
+    try {
+      Thread.sleep(msecs);
+    } catch (Exception e) {
+    }
+  }
+
+}
